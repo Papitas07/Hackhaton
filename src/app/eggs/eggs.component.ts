9970006af01d0cfeead002e4fb6db075fc34066d
Alexandre Cochet <alexcochet1@hotmail.fr>
2019-04-19 13:38:27 +0200

    Final

--- src/app/eggs/eggs.component.ts
+++ src/app/eggs/eggs.component.ts
@@ -25,45 +25,73 @@ export class EggsComponent implements OnInit {
 
   ngOnInit() {
 
+    
     this.eggs.getEggOne().subscribe(result=> {
       this.allMerchandEggs.push(result)
+      this.eggOne.priceMilk = 10
+      this.eggOne.priceChocolate = 23
+      this.eggOne.priceEgg = 15
       console.log(this.allMerchandEggs)
-
-
-    })
-    this.eggs.getEgg2().subscribe(result=> {
+      
+      
+      })
+      this.eggs.getEgg2().subscribe(result=> {
       this.allMerchandEggs.push(result)
-
-
-    })
-    this.eggs.getEgg3().subscribe(result=> {
+      this.egg2.priceMilk = 33 
+      this.egg2.priceChocolate = 24
+      this.egg2.priceEgg = 12
+      
+      
+      })
+      this.eggs.getEgg3().subscribe(result=> {
       this.allMerchandEggs.push(result)
-
-
-    })
-    this.eggs.getEgg4().subscribe(result=> {
+      this.egg3.priceMilk = 12
+      this.egg3.priceChocolate = 45
+      this.egg3.priceEgg = 24
+      
+      
+      })
+      this.eggs.getEgg4().subscribe(result=> {
       this.allMerchandEggs.push(result)
-
- 
+      this.egg4.priceMilk = 23
+      this.egg4.priceChocolate = 65
+      this.egg4.priceEgg = 11
+      
+      
+      
       this.eggs.getEgg5().subscribe(result=> {
-        this.allMerchandEggs.push(result)
-  
-  
+      this.allMerchandEggs.push(result)
+      this.egg5.priceMilk = 34
+      this.egg5.priceChocolate = 14
+      this.egg5.priceEgg = 12
+      
+      
+      
       })
       this.eggs.getEgg6().subscribe(result=> {
-        this.allMerchandEggs.push(result)
-  
-  
+      this.allMerchandEggs.push(result)
+      this.egg6.priceMilk = 14
+      this.egg6.priceChocolate= 29
+      this.egg6.priceEgg = 17
+      
+      
       })
       this.eggs.getEgg7().subscribe(result=> {
-        this.allMerchandEggs.push(result)
-  
-  
+      this.allMerchandEggs.push(result)
+      this.egg7.priceMilk = 36
+      this.egg7.priceChocolate = 32
+      this.egg7.priceEgg = 17
+      
+      
       })
       this.eggs.getEgg8().subscribe(result=> {
-        this.allMerchandEggs.push(result)
-  
-  
+      this.allMerchandEggs.push(result)
+      this.egg8.priceMilk = 21
+      this.egg8.priceChocolate = 32
+      this.egg8.priceEgg = 5
+      
+      
+      
       })
       this.eggs.getLegendaryEgg().subscribe(result=> {
         this.legendaryEgg = result
